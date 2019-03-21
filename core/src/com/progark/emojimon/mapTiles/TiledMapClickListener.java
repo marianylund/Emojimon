b0cec56b3474ad8e4223770f459242bddf8cb8b1
Sirikam <ben.dik@live.no>
2019-03-21 11:36:06 +0100

    merge for mvcskeleton

---
+++ core/src/com/progark/emojimon/mapTiles/TiledMapClickListener.java
@@ -0,0 +1,18 @@
+package com.progark.emojimon.mapTiles;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+public class TiledMapClickListener extends ClickListener {
+
+    private TiledMapActor actor;
+
+    public TiledMapClickListener(TiledMapActor actor) {
+        this.actor = actor;
+    }
+
+    @Override
+    public void clicked(InputEvent event, float x, float y) {
+        System.out.println(actor + " has been clicked.");
+    }
+}
