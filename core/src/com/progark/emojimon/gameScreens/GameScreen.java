a7efcfea0600bd8b709438469248d9db23b2fc46
Sirikam <ben.dik@live.no>
2019-03-21 09:51:24 +0100

    more work on the screens setup

--- core/src/com/progark/emojimon/gameScreens/GameScreen.java
+++ core/src/com/progark/emojimon/gameScreens/GameScreen.java
@@ -1,9 +1,12 @@
 package com.progark.emojimon.gameScreens;
 
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.progark.emojimon.Emojimon;
 
-public class GameScreen {
+import java.lang.reflect.Array;
+
+public class GameScreen implements Screen {
 
     final Emojimon game;
 
@@ -14,6 +17,42 @@ public class GameScreen {
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
 
     }
 }
--- core/src/com/progark/emojimon/gameScreens/MainMenuScreen.java
+++ core/src/com/progark/emojimon/gameScreens/MainMenuScreen.java
@@ -25,7 +25,7 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        
     }
 
     @Override
