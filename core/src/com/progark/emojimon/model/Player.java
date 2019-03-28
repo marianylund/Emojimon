bda5423e3c20b99ff572b63a2e29a1370cd7d8fc
Minh Ha Do <minhhd@stud.ntnu.no>
2019-03-28 09:59:00 +0100

    added logic for starting at bar #22

--- core/src/com/progark/emojimon/model/Player.java
+++ core/src/com/progark/emojimon/model/Player.java
@@ -14,8 +14,8 @@ public class Player {
     private int homeAreaStartIndex;
     private int homeAreaEndIndex;
     private boolean moveClockwise;
-    private MoveValidationStrategy moveValidationStrategy;
-    private CanClearStrategy canClearStrategy;
+    private MoveValidationStrategy moveValidationStrategy; //TODO: add factory pattern for strategy
+    private CanClearStrategy canClearStrategy; //TODO: add factory pattern for strategy
 
     public Player(int homeAreaStartIndex, int homeAreaEndIndex, boolean moveClockwise){
         this.homeAreaStartIndex = homeAreaStartIndex;
@@ -27,6 +27,32 @@ public class Player {
     //(currently only checking individual die, not combinations)
     public List<Move> getAvailableMoves(List<Die> dice, List<Position> positions, Position bar) {
         List<Move> moves = new ArrayList<Move>();
+
+        // check if player has pieces on the bar.
+        if (bar.getPieceCount() != 0 && this.equals(bar.getOwner())){
+            // check for possible moves from bar
+            int barIndex = positions.size() + 1;
+            for (int diceIndex = 0; diceIndex < dice.size(); diceIndex++){
+                int diceValue = dice.get(diceIndex).GetValue();
+                int endPositionIndex = moveClockwise ? (barIndex + diceValue) : (barIndex - diceValue);
+                Position endPosition = positions.get(endPositionIndex);
+
+                //apply move validation strategy to check if move is valid
+                if(moveValidationStrategy.isAvailableMove(bar, endPosition)){
+                    moves.add(new Move(barIndex, endPositionIndex));
+                }
+            }
+            if (moves.size() == 0){
+                // if pieces are in bar and no moves are available, return moves (which will be empty)
+                return moves;
+            }
+        }
+
+        //TODO: check if there are pieces at bar (strategy pattern). If they are, check if they're players pieces (bool)
+        //TODO: find all possible moves for bar ... apply moveValidationStrategy
+
+        //TODO: check if there are pieces at bar (strategy pattern). If they are, check if they're players pieces (bool)
+        //TODO: find all possible moves for bar ... apply moveValidationStrategy
 
         //find all possible moves for player given die values in dice
         for(int positionIndex = 0; positionIndex < positions.size(); positionIndex++){
